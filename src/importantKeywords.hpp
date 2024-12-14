255b6879c72ceb663f64d0b5500508e13d4f11bd
boom1401 <135537981+otsuliini@users.noreply.github.com>
2024-12-14 08:39:52 +0200

    cooking

---
+++ src/importantKeywords.hpp
@@ -0,0 +1,15 @@
+#ifndef IMPORTANTKEYWORDS_HPP
+#define IMPORTANTKEYWORDS_HPP
+#include <string>
+#include <vector>
+
+class ImportantKeywords{
+    public:
+
+    static const std::string dad;
+
+    
+};
+
+
+#endif 
